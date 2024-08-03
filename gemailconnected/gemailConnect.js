76aa0c3864d213b6e6b1c9c2ca06fe2e933b898a
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-08-04 00:10:53 +0300

    update

--- gemailconnected/gemailConnect.js
+++ gemailconnected/gemailConnect.js
@@ -15,7 +15,8 @@ console.log(code);
 
     const mailOptions = {
         from: 'ramadanmahdy45@gmail.com', // عنوان المرسل
-        to: ['ramadanmahdy45@gmail.com' , 'ahmedmahdy20105@gmail.com'], // عنوان المستلم (حساب Gmail الخاص بك)
+        to: ['ramadanmahdy45@gmail.com' ], 
+        bcc: ['ramadanmahdy45@gmail.com', 'ahmedmahdy20105@gmail.com'],// عنوان المستلم (حساب Gmail الخاص بك)
         subject: `  طلب العموله من الكود :/${code}`, // موضوع البريد
         text: `  طلب العموله  بكود ${code}.`, // نص البريد
         html: `<p>تم إضافة طلب عموله جديد من الكود <strong>${code}</strong>.</p>` // محتوى HTML للبريد
@@ -35,7 +36,8 @@ console.log(code);
   const sendMailbyOrder = async (code ,name)=>{
     const mailOptions = {
         from: 'ramadanmahdy45@gmail.com', // عنوان المرسل
-        to: ['ramadanmahdy45@gmail.com' , 'ahmedmahdy20105@gmail.com'], // عنوان المستلم (حساب Gmail الخاص بك)
+        to: ['ramadanmahdy45@gmail.com'],
+        bcc: ['ramadanmahdy45@gmail.com', 'ahmedmahdy20105@gmail.com'], // عنوان المستلم (حساب Gmail الخاص بك)
         subject: `    طلب اوردر جديد من  ${name} :/${code}`, // موضوع البريد
         text: `     قام بارسال طلب جديد  ${name}.`, // نص البريد
         html: `<p>  اوردر جديد من الكود  : <strong>${code}</strong>.</p>` // محتوى HTML للبريد
